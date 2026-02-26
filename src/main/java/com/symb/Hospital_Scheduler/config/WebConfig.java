57945539e6342cbf58693ed4223465907f70e97f
whoa12 <111991233+whoa12@users.noreply.github.com>
2026-02-26 21:49:38 +0530

    Update CORS configuration to allow Netlify origin

--- src/main/java/com/symb/Hospital_Scheduler/config/WebConfig.java
+++ src/main/java/com/symb/Hospital_Scheduler/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://splendorous-mochi-438c8a.netlify.app/")
                 .allowedMethods("GET", "POST");
     }
 }
